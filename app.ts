681b7271274943dbc6c87948fa3bda91d72b0d34
Ross O'Reilly <oreillyross@gmail.com>
2015-07-21 10:55:12 +0000

    on the road to Angular2

---
+++ app.ts
@@ -0,0 +1,29 @@
+/// <reference path="typings/angular2/angular2.d.ts" />
+
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+    
+    selector: 'my-app'
+})
+
+
+@View({
+    
+    template: '<h1>Hello {{name}}</h1>' 
+})
+
+class mainComponent {
+    name: string;
+    constructor() {
+        this.name = 'This works cool';
+    }
+}
+
+class pantryItem {
+    name: string;
+    quantity: integer;
+    measurmentType: object;
+}
+
+bootstrap(mainComponent);
