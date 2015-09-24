d828c692074ec90b0c7a128bf4a3c1c066b7a4d9
Ross O'Reilly <oreillyross@gmail.com>
2015-09-24 13:22:48 +0000

    sassify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,43 @@
-module.exports = {
-  entry: './src/app.js',
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+
+const sassLoaders = [
+  "css-loader",
+  "autoprefixer-loader?browsers=last 2 version",
+  "sass-loader?indentedSyntax=sass&includePaths[]=" + path.resolve(__dirname, "./src"),
+];
+
+const config = {
+  entry: {
+    app: ["./src/app"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"],
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract("style-loader", sassLoaders.join("!")),
+      },
+    ]
+  },
   output: {
-      filename: 'bundle.js'
-  }
+    filename: "[name].js",
+    path: path.join(__dirname, "./build"),
+    publicPath: "/build",
+  },
+  plugins: [
+    new ExtractTextPlugin("[name].css"),
+  ],
+  resolve: {
+    extensions: ["", ".js", ".sass"],
+    modulesDirectories: ["src", "node_modules"],
+  },
 };
 
+module.exports = config;
 
