990357af606f5c9ad89532bea135b0f8635fc043
Ross O'Reilly <oreillyross@gmail.com>
2015-12-04 09:49:24 +0000

    more tests

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,8 @@ module.exports = function(config) {
 
 
     // web server port
-    port: 9876,
+    //port: 9876,
+    port: 8080,
 
 
     // enable / disable colors in the output (reporters and logs)
