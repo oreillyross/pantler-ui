58891b8c9594dbdba57dee52148c8fe3d81dce8a
Ross O'Reilly <oreillyross@gmail.com>
2015-12-04 10:15:13 +0000

    add equality test examples

--- test/js/core/test.js
+++ test/js/core/test.js
@@ -1,6 +1,29 @@
-QUnit.test('Testing', function(assert) {
-    assert.equal(1,1, 'one is equal to one');
+"use strict";
+
+QUnit.test('Ok Testing', function(assert) {
     assert.ok(true, "true is truthy");
     assert.ok([], "is an empty array");
+    assert.ok("non empty", "non-empty string succeeds");
+    assert.ok("", "empty string fails");
+    assert.ok(null, "null fails");
+    assert.ok(undefined, "undefined fails");
+    
+    
+    
 
 } );
+
+QUnit.test("Equal Testing", function(assert) {
+  assert.equal(0,0, "Zero = Zero"); 
+  assert.strictEqual("", 0, "empty string = 0 succeeds");
+  assert.equal("", 0, "empty string = 0 succeeds");
+  assert.equal("three", 3, "Three = 3 equal fails");
+  
+});
+
+QUnit.test("Deep Equel is the best to use", function (assert) {
+  let obj = { foo: "Bar"};
+  
+  assert.deepEqual(obj, {foo: "Bar"}, "Two objects can be the same in value");    
+    
+});
